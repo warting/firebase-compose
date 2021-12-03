a3239a844fc11af343e85a7ed6583a40dbab911b
Stefan Wärting <stefan@warting.se>
2021-12-03 14:01:08 +0100

    Update build.gradle.kts

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -14,7 +14,8 @@ val PUBLISH_ARTIFACT_ID by extra("auth")
 
 apply(from = "${rootProject.projectDir}/gradle/publish-module.gradle")
 
-val composeVersion = "1.0.4"
+
+val composeVersion = "1.1.0-beta04"
 
 android {
     compileSdk = 31
